d48323507b4837be90c5794a182705510ee22855
lazarescu1 <lazarescu.sebi1@gmail.com>
2025-06-18 16:49:05 +0300

    changed the script to work better on my machine

--- autoconnect.sh
+++ autoconnect.sh
@@ -8,7 +8,15 @@ echo -e "${GREEN}Connecting to keyboard...${NC}"
 sleep 1
 
 bluetoothctl << EOF
+power on
+agent KeyboardOnly
+default-agent
+pairable on
+scan on
+pair DC:2C:26:3E:A2:FD
+trust DC:2C:26:3E:A2:FD
 connect DC:2C:26:3E:A2:FD
+quit
 EOF
 
 echo -e "${GREEN}Restarting kanata service...${NC}"
